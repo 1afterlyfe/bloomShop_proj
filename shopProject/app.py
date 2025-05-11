0eeae394cef8b53d3ddc013f4f1fb8e2cd81b7a2
1afterlyfe <161028758+1afterlyfe@users.noreply.github.com>
2025-05-11 16:24:15 +0300

    Update app.py

--- shopProject/app.py
+++ shopProject/app.py
@@ -15,9 +15,9 @@ app.config['SECRET_KEY'] = '123123'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'romaplay122456@gmail.com'
-app.config['MAIL_PASSWORD'] = 'bujk gujf qeaa favk'
-app.config['MAIL_DEFAULT_SENDER'] = 'romaplay122456@gmail.com'
+app.config['MAIL_USERNAME'] = '...'
+app.config['MAIL_PASSWORD'] = '...'
+app.config['MAIL_DEFAULT_SENDER'] = '...'
 
 db = SQLAlchemy(app)
 mail = Mail(app)
