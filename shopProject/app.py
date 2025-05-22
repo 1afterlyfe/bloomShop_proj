77de189930255462be1e99d2aa4bf906b3fa310d
1afterlyfe <161028758+1afterlyfe@users.noreply.github.com>
2025-05-22 10:16:05 +0300

    some fixes

--- shopProject/app.py
+++ shopProject/app.py
@@ -7,17 +7,18 @@ from functools import wraps
 from flask import g
 from sqlalchemy import func
 from sqlalchemy.orm import validates
+from sensData import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bloomshop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '123123'
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = '...'
-app.config['MAIL_PASSWORD'] = '...'
-app.config['MAIL_DEFAULT_SENDER'] = '...'
+app.config['MAIL_USERNAME'] = MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
+app.config['MAIL_DEFAULT_SENDER'] = MAIL_DEFAULT_SENDER
 
 db = SQLAlchemy(app)
 mail = Mail(app)
